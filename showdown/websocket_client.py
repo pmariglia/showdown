3878e1a7bc70b50bfbdea420f0a252303ffa0d3b
pmariglia <mariglia4423@gmail.com>
2024-06-28 08:46:46 -0400

    Better login failure message

--- showdown/websocket_client.py
+++ showdown/websocket_client.py
@@ -86,9 +86,9 @@ class PSWebsocketClient:
         if response.status_code == 200:
             if self.password:
                 response_json = json.loads(response.text[1:])
-                if not response_json['actionsuccess']:
-                    logger.error("Login Unsuccessful")
-                    raise LoginError("Could not log-in")
+                if "actionsuccess" not in response_json:
+                    logger.error("Login Unsuccessful: {}".format(response_json))
+                    raise LoginError("Could not log-in: {}".format(response_json))
 
                 assertion = response_json.get('assertion')
             else:
